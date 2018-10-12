417d6f516d70bf5235bdeb83eebb91b784c8708d
HaloWord <906602554@qq.com>
2018-10-12 23:03:35 +0800

    开始leetcode尝试

---
+++ src/main/java/com/leetcode/TwoSum.java
@@ -0,0 +1,61 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: zhuzz
+ * @description:
+ * @date: 2018/10/12 22:53
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int [] a = {2, 11, 5, 7};
+        int[] c = twoSum3(a,9);
+        System.out.println(c);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int [] a = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length ; i++) {
+            if(map.containsKey(target - nums[i])) {
+                a [0] = nums[i];
+                a [1] = map.get(target - nums[i]);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return a;
+    }
+
+    public static int[] twoSum2(int[] nums, int target) {
+        int[] indices = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(nums[i])) {
+                indices[0]=map.get(nums[i]);
+                indices[1]=i;
+                break;
+            } else {
+                map.put(target-nums[i],i);
+            }
+        }
+        return indices;
+    }
+
+    public static int[] twoSum3(int[] nums, int target) {
+        int pow2 =(int)Math.pow(2, 5), limit = pow2 / 2;
+        short[] positions = new short[pow2];
+        for(int aNum = 0; aNum < nums.length; aNum++) {
+            int complement = target - nums[aNum], complPos = positions[(complement + limit)% pow2];
+            if(complPos != 0) {
+                return new int[] { complPos + Short.MAX_VALUE / 2, aNum };
+            }
+            positions[(nums[aNum] + limit)% pow2]=(short)(aNum - Short.MAX_VALUE / 2);
+        }
+        return null;
+    }
+}
